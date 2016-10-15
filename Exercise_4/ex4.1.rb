7de6282ef4b56cc71056e6961e520705172f5499
Court <courtney.lee.osborn@gmail.com>
2016-10-15 08:16:59 +0000

    Added Exercise_4 folder containing four ex4 files

---
+++ Exercise_4/ex4.1.rb
@@ -0,0 +1,4 @@
+puts "Understanding the error message"
+
+puts "Line 14 contained the carpool_capacity variable in which there was an error."
+puts "This error indicated that the variable carpool_capacity was not defined correctly"
---
+++ Exercise_4/ex4.2.rb
@@ -0,0 +1,2 @@
+puts "4.0 is a floating point number. It is important because we don’t always want the number to be rounded down."
+puts "Sometimes it is important to know exactly how much we have i.e. when we are counting money." 
