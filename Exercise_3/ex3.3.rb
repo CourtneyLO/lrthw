103337f519cf4537e24742c85b71d07f81bdc7c4
Court <courtney.lee.osborn@gmail.com>
2016-10-15 07:16:55 +0000

    Added Exercise_3 folder including five ex3 files

---
+++ Exercise_3/ex3.3.rb
@@ -0,0 +1,4 @@
+puts "I have been alive for #{26 * 365 } days"
+puts "I have been alive for #{26 * 365 * 24 } hours"
+puts "I have been alive for #{26 * 365 * 24 * 60} minutes"
+puts "I have been alive for #{26 * 365 * 24 * 60 * 60 } seconds"
