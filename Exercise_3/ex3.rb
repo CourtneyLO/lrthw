a689a468e13cd47845b4e8f1d682bd3b91bd968a
Court <courtney.lee.osborn@gmail.com>
2016-10-15 07:36:22 +0000

    Made a change to ex3.rb

--- Exercise_3/ex3.rb
+++ Exercise_3/ex3.rb
@@ -1,9 +1,9 @@
-+ plus - addition 
-- minus - subtraction
-/ slash - division 
-* asterisk - multiplication 
-% percentage - modulus 
-< less-than - less than something 
-> greater-than - greater than something 
-<= less-than-equal - less than or equal to 
->= more-than-equal - more than or equal to 
+puts "+ plus - addition"
+puts "- minus - subtraction"
+puts "/ slash - division"
+puts "* asterisk - multiplication"
+puts "% percentage - modulus"
+puts "< less-than - less than something" 
+puts "> greater-than - greater than something" 
+puts "<= less-than-equal - less than or equal to"
+puts ">= more-than-equal - more than or equal to" 
