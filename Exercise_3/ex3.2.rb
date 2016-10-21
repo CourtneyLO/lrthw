dea11f81ebde4e4f944fa911830f552e8d36630d
Court <courtney.lee.osborn@gmail.com>
2016-10-21 08:32:33 +0000

    Modified Exercise_3

--- Exercise_3/ex3.2.rb
+++ Exercise_3/ex3.2.rb
@@ -1,36 +1,6 @@
-# prints a string 
-puts "I will now count my chickens:"
+# Find something you need to calculate and write a new .rb file that does it
 
-# prints a string and converts calculation into a string  
-puts "Hens #{25 + 30 / 6}"
-# prints a string and converts calculation into a string  
-puts "Roosters #{100 - 25 * 3 % 4}"
-
-# prints a string 
-puts "Now I will count the eggs:"
-
-#calculates the equation to return an answer  
-puts 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
-
-# prints a string 
-puts "Is it true that 3 + 2 < 5 -7?"
-
-#calculates the equation to return an answer
-puts 3 + 2 < 5 - 7
-
-# prints a string and converts calculation into a string 
-puts "What is 3 + 2? #{3 + 2}"
-# prints a string and converts calculation into a string 
-puts "What is 5 - 7? #{5 - 7}"
-
-# prints a string 
-puts "Oh, that's why it's false."
-# prints a string 
-puts "How about some more."
-
-# prints a string and converts calculation into a string - answer is a boolean
-puts "Is it greater? #{5 > -2}"
-# prints a string and converts calculation into a string - answer is a boolean
-puts "Is it greater or equal? #{5 >= -2}"
-# prints a string and converts calculation into a string - answer is a boolean
-puts "Is it less or equal? #{5 <= -2}"
+puts "I have been alive for #{26 * 365 } days"
+puts "I have been alive for #{26 * 365 * 24 } hours"
+puts "I have been alive for #{26 * 365 * 24 * 60} minutes"
+puts "I have been alive for #{26 * 365 * 24 * 60 * 60 } seconds"
